ae51734ad4972016645d8565f69e1025f1ef1701
Nick Moyer <nick.moyer@awh.net>
2019-12-27 14:06:31 -0500

    Added code to handle checkout

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: '3c58bskmr44mpfnj',
+    publicKey: '4m6jx82rv7pqv7km',
+    privateKey: '57c4ba9417719cc25e9b704301cb3498'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  var newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
